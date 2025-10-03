e35452c79fc2746b9572fdae185bb4fe6d42aaf7
Sania Debbarma <saniadebbarma792@gmail.com>
2025-10-03 22:41:58 +0530

    Add files via upload

---
+++ Test4.java
@@ -0,0 +1,16 @@
+public class Test4 {
+    public static void main (String[]args){
+        int[] arr = {4, 3, 2, 1};
+        for (int i = 0; i < arr.length -1; i++){
+            for (int j = 0; j < arr.length -1 - i; j++){
+                if (arr[j] < arr[j + 1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println(arr[0]);
+    }
+    
+}
